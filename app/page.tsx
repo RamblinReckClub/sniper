27e148e8164d9fb875779af77ccb182625265eed
Gal Ovadia <gal.ovadia@gatech.edu>
2024-11-04 11:23:20 -0500

    remove card

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import ProductCard from './components/ProductCard/ProductCard'
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main> 
+    <main>
       <h1>hello world</h1>
-      <Link href="/users">Users</Link>
-      <ProductCard />
     </main>
-  )
+  );
 }
